f1e920a6a33d901d0c56e2357ee6f41afbcb8ad9
Shimada Ryuga <shimadaryuga@gmail.com>
2024-08-16 21:54:07 +0900

    feat: ログイン・ユーザー登録後のリダイレクト先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_id, :username])
     devise_parameter_sanitizer.permit(:account_update, keys: [:profile_id, :username])
+  end
+
+  private
+  # ログイン後のリダイレクト先
+  def after_sign_in_path_for(resource_or_scope)
+    top_path  #ここを好きなパスに変更
+  end
+
+  # ログアウト後のリダイレクト先
+  def after_sign_out_path_for(resource_or_scope)
+    root_path #ここを好きなパスに変更
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index new]
   def index; end
-
 end
