07ec480e3d259e07f39e34e645fdfa7531047832
Shimada Ryuga <shimadaryuga@gmail.com>
2024-08-18 17:27:46 +0900

    feat: ヤケクソのファイル削除その2

--- db/migrate/20240817075200_add_mbti_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddMbtiToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :mbti, :string
-  end
-end
--- db/migrate/20240817075415_mbtis.rb
+++
@@ -1,5 +0,0 @@
-class Mbtis < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :mbtis
-  end
-end
